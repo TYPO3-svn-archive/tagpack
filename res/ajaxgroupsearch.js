c36a590def5c74928b0285180c6970c7158e0c8a
cybercr@ft <cybercr@ft@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-02-02 20:20:21 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tagpack/tags/tagpack_0_4_0@16322 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ res/ajaxgroupsearch.js
@@ -0,0 +1,176 @@
+/**
+ * manages one ajax-search field 
+ **/
+function tx_tagpack_ajaxsearch_observer(el,creator,params) {
+	/**
+	 * add keyboard event listener on focus
+	 * onfocus is already an event handler the "oldschool" attribute way
+	 **/
+	this.onfocus = function() {
+		
+		if (!this.vcListener) {
+			if (!this.results.empty())
+				this.results.style.display = 'block';
+			this.vcListener = this.valChanged.bindAsEventListener(this);
+			this.el.observe('keydown',this.vcListener);
+		}
+	}
+
+	/** 
+	 * remove keyboard listener on blur
+	 **/
+	this.onblur = function(e) {
+		if (this.vcListener) {
+			this.el.stopObserving('keydown',this.vcListener);
+			this.vcListener = null;			
+		}
+		this.hideResults();
+	}
+
+	/**
+	 * helper method add a little timeout so the character arrives at the input
+	 **/
+	this.valChanged = function(e) {		
+		window.setTimeout(this.valChangedDelayed.bind(this),20);
+	}
+
+	/**
+	 * checks if there is enough input to perform the search
+	 **/
+	this.valChangedDelayed = function() {				
+		var v = this.el.value;
+		window.status = v;
+		if (v.length >= this.params.startLength) {
+			if(this.lastVal != v) {
+				// yeah, fancy loading icons from www.ajaxload.info
+				this.el.addClassName('loading');
+				this.request(v);
+				this.lastVal = v;
+			}
+		} else {
+			this.results.innerHTML = '';
+		}
+	}
+
+	/**
+	 * submits the ajax search request 
+	 **/
+	this.request = function(query) {
+		new Ajax.Updater(
+			this.results,
+			window.tx_tagpack_ajaxsearch_server,
+			{
+				method:'get',
+				parameters:{'function':'groupsearch','id':this.el.id,'value':query,'pid':this.el.title.replace(/Tags/g,'')},
+				onSuccess: this.showResults.bind(this),
+				onFailure: function(){ alert('error in request!') }
+			}
+		);
+	}
+
+
+	this.showResults = function() {
+		this.el.removeClassName('loading');
+		this.results.style.display = 'block';
+		if (!this.resultsListener) {
+			this.resultsListener =  this.resultsKeypressed.bindAsEventListener(this);
+			Event.observe(this.keyObserver, 'keydown', this.resultsListener, false);
+		}
+	}
+
+	this.hideResults = function() {
+		if (this.resultsListener) {
+			Event.stopObserving(this.keyObserver, 'keydown', this.resultsListener);
+			this.resultsListener = null;
+		}
+		// this timeout is super important, else the result browser will disappear before the clicks will be triggered
+		window.setTimeout((function () {this.results.hide()}).bind(this),100);
+	}
+
+	/**
+	 * key handling for browsing the result list
+	 **/
+	this.resultsKeypressed = function (e) {
+		var oldCurrent = this.current;
+		switch (e.keyCode) {
+			case Event.KEY_DOWN:
+				// take the next or the first
+				this.current = this.current ? this.current.next() : this.results.down('li');
+				break;
+			case Event.KEY_UP:
+				// take the previos or the first
+				this.current = this.current ? this.current.previous() : this.results.down('li');
+				break;
+			
+			case 10: // prototype doesn't care for *ix*
+			case Event.KEY_RETURN:
+				if (this.current) {
+					this.current.down('a').onclick();
+					this.current = false;
+					this.el.value='';
+					this.el.blur();
+					this.el.focus();
+					this.lastVal = false;
+				} else {
+				    setFormValueFromBrowseWin(this.el.id.replace(/_ajaxsearch/g,''),'new_'+this.el.value,this.el.value,'');
+				    this.el.value='';
+				    this.el.blur();
+				    this.el.focus();
+				    this.lastVal = false;
+				}
+				Event.stop(e);
+				break;
+
+			case Event.KEY_HOME:
+				this.current = this.results.down('li');
+				Event.stop(e);
+				break;
+			case Event.KEY_END:
+				this.current = this.results.down('li');
+				this.current = this.current.nextSiblings().last();
+				Event.stop(e);
+				break;
+		}
+
+		if (oldCurrent)
+			oldCurrent.removeClassName('focus');
+
+		if (this.current) 
+			this.current.addClassName('focus');		
+	}
+
+	/**
+	 * "constructor"
+	 */
+	this.init = function () {	
+		this.el = $(el);	
+		this.parent = $(el.parentNode);
+		this.keyObserver = (document.all && !window.opera) ? window.document : window;
+		this.results = $(this.el.id+'_results');
+		this.creator = creator;
+		this.params = params;
+		this.params.startLength = this.params.startLength ? this.params.startLength : 3;
+		this.vcListener = 0;
+		this.el.observe('blur',this.onblur.bindAsEventListener(this));		
+	}
+	
+	this.init();	
+}
+
+/**
+ * creates and manages the tx_tagpack_ajaxsearch_observer objects
+ */
+function tx_tagpack_ajaxsearch_lazyCreate() {
+	this.els = new Array();
+	this.get = function (el,params) {		
+		var id = el.id;
+		if (!this.els[id]) {	
+			this.els[id] = new tx_tagpack_ajaxsearch_observer(el,this,params);
+		}
+		return this.els[id];
+	}
+}
+/**
+ * THE one global intance of tx_tagpack_ajaxsearch_lazyCreate
+ */
+window.tx_tagpack_ajaxsearch_lazyCreator = new tx_tagpack_ajaxsearch_lazyCreate();
