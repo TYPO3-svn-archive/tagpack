d005141f5b64f93676c5e5ad944e5ebccac7e65b
cybercr@ft <cybercr@ft@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-02-06 12:46:04 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tagpack/tags/tagpack_0_5_2@16635 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "tagpack"
 #
-# Auto generated 06-02-2009 11:58
+# Auto generated 06-02-2009 12:47
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.5.1',
+	'version' => '0.5.2',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:39:{s:9:"ChangeLog";s:4:"b64d";s:10:"README.txt";s:4:"ee2d";s:38:"class.tx_tagpack_ajaxsearch_client.php";s:4:"ce48";s:38:"class.tx_tagpack_ajaxsearch_server.php";s:4:"36d4";s:28:"class.tx_tagpack_realurl.php";s:4:"ac2e";s:37:"class.tx_tagpack_tceforms_addtags.php";s:4:"005d";s:21:"ext_conf_template.txt";s:4:"8af7";s:12:"ext_icon.gif";s:4:"f4d1";s:17:"ext_localconf.php";s:4:"2067";s:14:"ext_tables.php";s:4:"2c95";s:14:"ext_tables.sql";s:4:"922c";s:24:"icon_tx_tagpack_tags.gif";s:4:"f4d1";s:13:"locallang.xml";s:4:"5c1d";s:16:"locallang_db.xml";s:4:"709f";s:7:"tca.php";s:4:"0800";s:14:"doc/manual.sxw";s:4:"f7ea";s:19:"doc/wizard_form.dat";s:4:"c7aa";s:20:"doc/wizard_form.html";s:4:"74dc";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"4ee8";s:14:"mod1/index.php";s:4:"da77";s:18:"mod1/locallang.xml";s:4:"e61d";s:22:"mod1/locallang_mod.xml";s:4:"c48c";s:19:"mod1/moduleicon.gif";s:4:"8074";s:28:"pi1/class.tx_tagpack_pi1.php";s:4:"c6d0";s:28:"pi2/class.tx_tagpack_pi2.php";s:4:"338e";s:28:"pi3/class.tx_tagpack_pi3.php";s:4:"3651";s:17:"pi3/locallang.xml";s:4:"9028";s:23:"res/ajaxgroupsearch.css";s:4:"3e01";s:22:"res/ajaxgroupsearch.js";s:4:"1390";s:15:"res/loading.gif";s:4:"e059";s:24:"res/magnifying-glass.gif";s:4:"5b28";s:24:"res/magnifying-glass.xcf";s:4:"ffd5";s:29:"static/tagcloud/constants.txt";s:4:"c924";s:25:"static/tagcloud/setup.txt";s:4:"ddd0";s:34:"static/tagnomination/constants.txt";s:4:"d41d";s:30:"static/tagnomination/setup.txt";s:4:"7d41";s:32:"static/taggeditems/constants.txt";s:4:"9e61";s:28:"static/taggeditems/setup.txt";s:4:"c425";}',
+	'_md5_values_when_last_written' => 'a:39:{s:9:"ChangeLog";s:4:"954b";s:10:"README.txt";s:4:"ee2d";s:38:"class.tx_tagpack_ajaxsearch_client.php";s:4:"ce48";s:38:"class.tx_tagpack_ajaxsearch_server.php";s:4:"36d4";s:28:"class.tx_tagpack_realurl.php";s:4:"ac2e";s:37:"class.tx_tagpack_tceforms_addtags.php";s:4:"005d";s:21:"ext_conf_template.txt";s:4:"8af7";s:12:"ext_icon.gif";s:4:"f4d1";s:17:"ext_localconf.php";s:4:"2067";s:14:"ext_tables.php";s:4:"2c95";s:14:"ext_tables.sql";s:4:"922c";s:24:"icon_tx_tagpack_tags.gif";s:4:"f4d1";s:13:"locallang.xml";s:4:"5c1d";s:16:"locallang_db.xml";s:4:"709f";s:7:"tca.php";s:4:"0800";s:14:"doc/manual.sxw";s:4:"f7ea";s:19:"doc/wizard_form.dat";s:4:"c7aa";s:20:"doc/wizard_form.html";s:4:"74dc";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"4ee8";s:14:"mod1/index.php";s:4:"da77";s:18:"mod1/locallang.xml";s:4:"e61d";s:22:"mod1/locallang_mod.xml";s:4:"c48c";s:19:"mod1/moduleicon.gif";s:4:"8074";s:28:"pi1/class.tx_tagpack_pi1.php";s:4:"8265";s:28:"pi2/class.tx_tagpack_pi2.php";s:4:"338e";s:28:"pi3/class.tx_tagpack_pi3.php";s:4:"3651";s:17:"pi3/locallang.xml";s:4:"9028";s:23:"res/ajaxgroupsearch.css";s:4:"3e01";s:22:"res/ajaxgroupsearch.js";s:4:"1390";s:15:"res/loading.gif";s:4:"e059";s:24:"res/magnifying-glass.gif";s:4:"5b28";s:24:"res/magnifying-glass.xcf";s:4:"ffd5";s:29:"static/tagcloud/constants.txt";s:4:"c924";s:25:"static/tagcloud/setup.txt";s:4:"ddd0";s:34:"static/tagnomination/constants.txt";s:4:"d41d";s:30:"static/tagnomination/setup.txt";s:4:"7d41";s:32:"static/taggeditems/constants.txt";s:4:"9e61";s:28:"static/taggeditems/setup.txt";s:4:"bb9a";}',
 	'suggests' => array(
 	),
 );
