50c63d69d4d8e68c84c94aafab09e60bfebaadd3
cybercr@ft <cybercr@ft@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-02-06 23:10:07 +0000

    Tagged Version 0.5.5
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tagpack/tags/tagpack_0_5_5@16733 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "tagpack"
+#
+# Auto generated 06-02-2009 23:57
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Tag Pack',
+	'description' => 'All purpose tagging suite. Use tags for almost any allowed table without having to create new DB fields for each of them. Create multifunctional tag clouds using surf or filter mode together with time based settings, a tag breadcrumb menu and a searchbox with autocompletion.',
+	'category' => '',
+	'shy' => 0,
+	'version' => '0.5.5',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => 'mod1',
+	'state' => 'alpha',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'JoH asenau',
+	'author_email' => 'info@cybercraft.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:39:{s:9:"ChangeLog";s:4:"89a9";s:10:"README.txt";s:4:"ee2d";s:38:"class.tx_tagpack_ajaxsearch_client.php";s:4:"ce48";s:38:"class.tx_tagpack_ajaxsearch_server.php";s:4:"36d4";s:28:"class.tx_tagpack_realurl.php";s:4:"ac2e";s:37:"class.tx_tagpack_tceforms_addtags.php";s:4:"005d";s:21:"ext_conf_template.txt";s:4:"8af7";s:12:"ext_icon.gif";s:4:"f4d1";s:17:"ext_localconf.php";s:4:"2067";s:14:"ext_tables.php";s:4:"2c95";s:14:"ext_tables.sql";s:4:"922c";s:24:"icon_tx_tagpack_tags.gif";s:4:"f4d1";s:13:"locallang.xml";s:4:"5c1d";s:16:"locallang_db.xml";s:4:"709f";s:7:"tca.php";s:4:"0800";s:28:"pi1/class.tx_tagpack_pi1.php";s:4:"5d16";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"4ee8";s:14:"mod1/index.php";s:4:"da77";s:18:"mod1/locallang.xml";s:4:"e61d";s:22:"mod1/locallang_mod.xml";s:4:"c48c";s:19:"mod1/moduleicon.gif";s:4:"8074";s:28:"pi3/class.tx_tagpack_pi3.php";s:4:"5668";s:17:"pi3/locallang.xml";s:4:"9028";s:28:"pi2/class.tx_tagpack_pi2.php";s:4:"338e";s:14:"doc/manual.sxw";s:4:"f7ea";s:19:"doc/wizard_form.dat";s:4:"c7aa";s:20:"doc/wizard_form.html";s:4:"74dc";s:23:"res/ajaxgroupsearch.css";s:4:"3e01";s:22:"res/ajaxgroupsearch.js";s:4:"1390";s:15:"res/loading.gif";s:4:"e059";s:24:"res/magnifying-glass.gif";s:4:"5b28";s:24:"res/magnifying-glass.xcf";s:4:"ffd5";s:34:"static/tagnomination/constants.txt";s:4:"d41d";s:30:"static/tagnomination/setup.txt";s:4:"7d41";s:32:"static/taggeditems/constants.txt";s:4:"9e61";s:28:"static/taggeditems/setup.txt";s:4:"dee7";s:29:"static/tagcloud/constants.txt";s:4:"c924";s:25:"static/tagcloud/setup.txt";s:4:"165b";}',
+	'suggests' => array(
+	),
+);
+
+?>
