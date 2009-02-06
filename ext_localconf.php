ef1d3ed8e2ac33b700ac87b378c608db083b51da
cybercr@ft <cybercr@ft@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-02-06 12:41:39 +0000

    Tagged Version 0.5.2
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tagpack/tags/tagpack_0_5_2@16625 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_localconf.php
@@ -0,0 +1,23 @@
+<?php
+if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+
+$lConf = unserialize($_EXTCONF);
+t3lib_extMgm::addPageTSConfig('
+	tx_tagpack_tags.taggedTables = '.$lConf['taggedTables'].'
+	tx_tagpack_tags.getTagsFromPid = '.$lConf['getTagsFromPid'].'
+');
+
+t3lib_extMgm::addUserTSConfig('
+	options.saveDocNew.tx_tagpack_tags=1
+');
+
+t3lib_extMgm::addPItoST43($_EXTKEY,'pi1/class.tx_tagpack_pi1.php','_pi1','',1);
+t3lib_extMgm::addPItoST43($_EXTKEY,'pi2/class.tx_tagpack_pi2.php','_pi2','',1);
+t3lib_extMgm::addPItoST43($_EXTKEY,'pi3/class.tx_tagpack_pi3.php','_pi3','list_type',1);
+
+$GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tceforms.php']['getMainFieldsClass'][] = 'EXT:tagpack/class.tx_tagpack_tceforms_addtags.php:tx_tagpack_tceforms_addtags';
+$GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tceforms.php']['getSingleFieldClass'][] = 'EXT:tagpack/class.tx_tagpack_tceforms_addtags.php:tx_tagpack_tceforms_addtags';
+$GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'EXT:tagpack/class.tx_tagpack_tceforms_addtags.php:tx_tagpack_tceforms_addtags';
+$GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass'][] = 'EXT:tagpack/class.tx_tagpack_tceforms_addtags.php:tx_tagpack_tceforms_addtags';
+
+?>
