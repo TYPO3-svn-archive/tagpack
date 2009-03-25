00b253eee790478b7d2eb5469c9876ed85bd5196
cybercr@ft <cybercr@ft@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-03-25 18:35:40 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tagpack/trunk@18382 735d13b6-9817-0410-8766-e36946ffe9aa

--- mod1/index.php
+++ mod1/index.php
@@ -285,7 +285,7 @@
 
 		function makeResultlist($tab) {
 			if($this->tpm['tagname'][$tab]) {
-				$resultList = tx_tagpack_api::getTagDataByTagName($this->tpm['tagname'][$tab],t3lib_div::trimExplode(',',$this->tpm['container_page'][$tab]));
+				$resultList = tx_tagpack_api::getTagDataByTagName($this->tpm['tagname'][$tab],implode(',',$this->tpm['container_page'][$tab]));
 			}
 			t3lib_div::debug($resultList);
 		} 
