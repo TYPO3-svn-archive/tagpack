ab0e1c82218419deac940e3ee8b1a1d5f4b1f498
cybercr@ft <cybercr@ft@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-12-04 10:19:32 +0000

    Tagged Version 0.10.6
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tagpack/tags/tagpack_0_10_6@27324 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "tagpack"
+#
+# Auto generated 04-12-2009 10:17
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Tag Pack',
+	'description' => 'All purpose tagging suite. Use tags for almost any allowed table without having to create new DB fields for each of them. Create multifunctional tag clouds using surf or filter mode together with time based settings, a tag breadcrumb menu and a searchbox with autocompletion.',
+	'category' => '',
+	'shy' => 0,
+	'version' => '0.10.6',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => 'mod1',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'JoH asenau',
+	'author_email' => 'info@cybercraft.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:98:{s:9:"ChangeLog";s:4:"5d07";s:10:"README.txt";s:4:"ee2d";s:38:"class.tx_tagpack_ajaxsearch_client.php";s:4:"a81d";s:38:"class.tx_tagpack_ajaxsearch_server.php";s:4:"0165";s:28:"class.tx_tagpack_realurl.php";s:4:"11da";s:37:"class.tx_tagpack_tceforms_addtags.php";s:4:"19f2";s:21:"class.ux_SC_dummy.php";s:4:"24ea";s:21:"ext_conf_template.txt";s:4:"3c14";s:12:"ext_icon.gif";s:4:"f4d1";s:17:"ext_localconf.php";s:4:"7199";s:14:"ext_tables.php";s:4:"afa5";s:14:"ext_tables.sql";s:4:"9b99";s:30:"icon_tx_tagpack_categories.gif";s:4:"f4d1";s:24:"icon_tx_tagpack_tags.gif";s:4:"f4d1";s:13:"locallang.xml";s:4:"9155";s:16:"locallang_db.xml";s:4:"85f8";s:7:"tca.php";s:4:"e42b";s:14:"doc/manual.sxw";s:4:"db57";s:19:"doc/wizard_form.dat";s:4:"c7aa";s:20:"doc/wizard_form.html";s:4:"74dc";s:28:"lib/class.tx_tagpack_api.php";s:4:"8604";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"fc0a";s:14:"mod1/index.php";s:4:"54cb";s:18:"mod1/locallang.xml";s:4:"3872";s:22:"mod1/locallang_mod.xml";s:4:"f72d";s:19:"mod1/moduleicon.gif";s:4:"f4d1";s:24:"mod1/css/lightwindow.css";s:4:"ea70";s:23:"mod1/css/tagmanager.css";s:4:"d060";s:18:"mod1/icons/add.gif";s:4:"408a";s:26:"mod1/icons/button_down.gif";s:4:"fa54";s:26:"mod1/icons/button_hide.gif";s:4:"fba8";s:26:"mod1/icons/button_left.gif";s:4:"cdec";s:27:"mod1/icons/button_right.gif";s:4:"5573";s:28:"mod1/icons/button_unhide.gif";s:4:"fde9";s:24:"mod1/icons/button_up.gif";s:4:"0cc7";s:20:"mod1/icons/clear.gif";s:4:"cc11";s:19:"mod1/icons/down.gif";s:4:"b8a8";s:19:"mod1/icons/edit.gif";s:4:"55bd";s:20:"mod1/icons/edit2.gif";s:4:"3248";s:22:"mod1/icons/edit2_d.gif";s:4:"2717";s:22:"mod1/icons/edit2_h.gif";s:4:"ddc9";s:22:"mod1/icons/edit_fe.gif";s:4:"336a";s:24:"mod1/icons/edit_file.gif";s:4:"a68b";s:24:"mod1/icons/edit_page.gif";s:4:"a055";s:26:"mod1/icons/edit_rtewiz.gif";s:4:"d8b4";s:25:"mod1/icons/editaccess.gif";s:4:"7bdb";s:22:"mod1/icons/garbage.gif";s:4:"7fbe";s:21:"mod1/icons/level1.gif";s:4:"443d";s:21:"mod1/icons/level2.gif";s:4:"d7a2";s:21:"mod1/icons/level3.gif";s:4:"47d3";s:21:"mod1/icons/level4.gif";s:4:"2271";s:20:"mod1/icons/minus.gif";s:4:"de77";s:26:"mod1/icons/minusbottom.gif";s:4:"5f7a";s:24:"mod1/icons/minusonly.gif";s:4:"362b";s:23:"mod1/icons/minustop.gif";s:4:"f47e";s:21:"mod1/icons/new_el.gif";s:4:"591c";s:24:"mod1/icons/new_level.gif";s:4:"7fcf";s:19:"mod1/icons/plus.gif";s:4:"d67c";s:25:"mod1/icons/plusbottom.gif";s:4:"9791";s:23:"mod1/icons/plusonly.gif";s:4:"f127";s:22:"mod1/icons/plustop.gif";s:4:"6d51";s:23:"mod1/icons/shortcut.gif";s:4:"7546";s:17:"mod1/icons/up.gif";s:4:"822e";s:28:"mod1/images/ajax-loading.gif";s:4:"b6f3";s:26:"mod1/images/arrow-down.gif";s:4:"2694";s:24:"mod1/images/arrow-up.gif";s:4:"020a";s:27:"mod1/images/background1.png";s:4:"6ff3";s:24:"mod1/images/black-70.png";s:4:"703c";s:21:"mod1/images/black.png";s:4:"1cf6";s:31:"mod1/images/buttongreen_off.png";s:4:"89af";s:30:"mod1/images/buttongreen_on.png";s:4:"edeb";s:29:"mod1/images/buttonred_off.png";s:4:"1918";s:28:"mod1/images/buttonred_on.png";s:4:"b24b";s:25:"mod1/images/nextlabel.gif";s:4:"485d";s:30:"mod1/images/pattern_148-70.png";s:4:"e04d";s:27:"mod1/images/pattern_148.gif";s:4:"4d56";s:25:"mod1/images/prevlabel.gif";s:4:"d935";s:26:"mod1/images/screenback.png";s:4:"4ba3";s:28:"mod1/images/submitbutton.png";s:4:"51a7";s:18:"mod1/js/effects.js";s:4:"7808";s:22:"mod1/js/lightwindow.js";s:4:"2f3d";s:27:"mod1/js/tabMenuFunctions.js";s:4:"5237";s:28:"pi1/class.tx_tagpack_pi1.php";s:4:"b5ab";s:28:"pi2/class.tx_tagpack_pi2.php";s:4:"338e";s:28:"pi3/class.tx_tagpack_pi3.php";s:4:"4c16";s:17:"pi3/locallang.xml";s:4:"9028";s:23:"res/ajaxgroupsearch.css";s:4:"89ee";s:22:"res/ajaxgroupsearch.js";s:4:"2986";s:15:"res/loading.gif";s:4:"e059";s:24:"res/magnifying-glass.gif";s:4:"5b28";s:24:"res/magnifying-glass.xcf";s:4:"ffd5";s:29:"static/tagcloud/constants.txt";s:4:"de0a";s:25:"static/tagcloud/setup.txt";s:4:"3d8a";s:32:"static/taggeditems/constants.txt";s:4:"9e61";s:28:"static/taggeditems/setup.txt";s:4:"dee7";s:34:"static/tagnomination/constants.txt";s:4:"d41d";s:30:"static/tagnomination/setup.txt";s:4:"7d41";}',
+	'suggests' => array(
+	),
+);
+
+?>
---
+++ pi3/class.tx_tagpack_pi3.php
@@ -0,0 +1,283 @@
+<?php
+	/***************************************************************
+	*  Copyright notice
+	*
+	*  (c) 2008 JoH asenau <info@cybercraft.de>
+	*  All rights reserved
+	*
+	*  This script is part of the TYPO3 project. The TYPO3 project is
+	*  free software; you can redistribute it and/or modify
+	*  it under the terms of the GNU General Public License as published by
+	*  the Free Software Foundation; either version 2 of the License, or
+	*  (at your option) any later version.
+	*
+	*  The GNU General Public License can be found at
+	*  http://www.gnu.org/copyleft/gpl.html.
+	*
+	*  This script is distributed in the hope that it will be useful,
+	*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+	*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	*  GNU General Public License for more details.
+	*
+	*  This copyright notice MUST APPEAR in all copies of the script!
+	***************************************************************/
+
+	require_once(PATH_tslib.'class.tslib_pibase.php');
+
+
+	/**
+	 * Plugin 'Tag Nominations' for the 'tagpack' extension.
+	 *
+	 * @author JoH asenau <info@cybercraft.de>
+	 * @package TYPO3
+	 * @subpackage tx_tagpack
+	 */
+	class tx_tagpack_pi3 extends tslib_pibase {
+		var $prefixId = 'tx_tagpack_pi3';
+		// Same as class name
+		var $scriptRelPath = 'pi3/class.tx_tagpack_pi3.php'; // Path to this script relative to the extension dir.
+		var $extKey = 'tagpack'; // The extension key.
+
+		/**
+ * The main method of the PlugIn
+ *
+ * @param	string		$content: The PlugIn content
+ * @param	array		$conf: The PlugIn configuration
+ * @return	The		content that is displayed on the website
+ */
+		function main($content, $conf) {
+			$this->conf = $conf;
+			$this->pi_loadLL();
+			$this->pi1Vars = t3lib_div::_GP('tx_tagpack_pi1');
+			$tagUidArray = t3lib_div::intExplode(',', $this->pi1Vars['uid']);
+			foreach($tagUidArray as $key => $value) {
+				if ($value == t3lib_div::_GET('tx_tagpack_pi3_removeItems')) {
+					unset($tagUidArray[$key]);
+				}
+			}
+			$tagUid = implode(',', $tagUidArray);
+			$tagUid = $tagUid ? $tagUid :
+			 0;
+			$conf['renderObj.']['10'] = 'HTML';
+			$tags = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows(
+			'*',
+				'tx_tagpack_tags',
+				'uid IN('.$tagUid.')
+				AND NOT deleted
+				AND NOT hidden
+				AND pid>0',
+				'',
+				'name ASC' );
+			if ($conf['taggedElements.']['enabledContent'] && count($tags)) {
+				$conf['renderObj.']['10.']['value'] .= $this->makeElementList('tt_content', $conf, $tags, $tagUid);
+			}
+			if ($conf['taggedElements.']['enabledRecords'] && count($tags)) {
+				$enabledRecords = t3lib_div::trimexplode(',', $conf['taggedElements.']['enabledRecords']);
+				foreach($enabledRecords as $table) {
+					$tables = $GLOBALS['TYPO3_DB']->admin_get_tables();
+					if (($tables[$table]['Name'] == $table || $tables[$table] == $table)) {
+						$conf['renderObj.']['10.']['value'] .= $this->makeElementList($table, $conf, $tags, $tagUid);
+					}
+				}
+			}
+			return $this->cObj->cObjGetSingle($conf['renderObj'], $conf['renderObj.']);
+		}
+
+		/**
+ * The main method of the PlugIn
+ *
+ * @param	string		$content: The PlugIn content
+ * @param	array		$conf: The PlugIn configuration
+ * @param	[type]		$tags: ...
+ * @param	[type]		$tagUid: ...
+ * @return	The		content that is displayed on the website
+ */
+		function makeElementList($table, $conf, $tags, $tagUid) {
+
+			$sortingTime = $conf['taggedElements.']['timeFields.'][$table] ? $conf['taggedElements.']['timeFields.'][$table] : 'tstamp';
+
+			if ($tagUid) {
+				$tagsSelected = ' AND tx_tagpack_tags_relations_mm.uid_local IN('.$tagUid.')';
+			}
+
+			if ($this->pi1Vars['from'] && $conf['taggedElements.']['timeFields.'][$table]) {
+				$fromTime = ' BETWEEN '.(strtotime($this->pi1Vars['from'])).' AND '.(strtotime($this->pi1Vars['from'])+(3600 * 24)-1);
+				$calendarSettings = ' AND '.$table.'.'.$conf['taggedElements.']['timeFields.'][$table].$fromTime;
+			}
+
+			if ($this->pi1Vars['from'] && $this->pi1Vars['to'] && $conf['taggedElements.']['timeFields.'][$table]) {
+				$fromTime = ' BETWEEN '.(strtotime($this->pi1Vars['from'])).' AND '.(strtotime($this->pi1Vars['to'])+(3600 * 24)-1);
+				$calendarSettings = ' AND '.$table.'.'.$conf['taggedElements.']['timeFields.'][$table].$fromTime;
+			}
+
+			if ($this->pi1Vars['searchWord'] && $conf['taggedElements.']['searchFields.'][$table]) {
+				$searchFieldArray = t3lib_div::trimexplode(',', $conf['taggedElements.']['searchFields.'][$table]);
+				foreach($searchFieldArray as $searchField) {
+					$searchSettings .= $searchSettings ? (' OR '.$table.'.'.$searchField.' LIKE '.$GLOBALS['TYPO3_DB']->fullQuoteStr('%'.$this->pi1Vars['searchWord'].'%',$table)) :
+					($table.'.'.$searchField.' LIKE '.$GLOBALS['TYPO3_DB']->fullQuoteStr('%'.$this->pi1Vars['searchWord'].'%',$table));
+				}
+				$searchSettings = $searchSettings ? ' AND ('.$searchSettings.')' :
+				 '';
+			}
+
+			if ($conf['taggedElements.']['additionalFilters.'][$table.'.']) {
+				$filters = $conf['taggedElements.']['additionalFilters.'][$table.'.'];
+				foreach($filters as $fieldName => $filterSettings) {
+					$getVar = t3lib_div::_GET($filterSettings['GETvar']);
+					if (is_array($getVar) && !$getVar[$filterSettings['GETvar.']['key']]) {
+						$getVar = false;
+					}
+					else if($getVar[$filterSettings['GETvar.']['key']]) {
+						$getVars[] = $getVar[$filterSettings['GETvar.']['key']];
+					}
+				}
+			}
+			
+			if ($tagUid) {
+				if ($conf['taggedElements.']['additionalFilters.'][$table.'.'] && count($getVars)) {
+					$limit = '';
+				} else {
+					$limit = $conf['taggedElements.']['maxItems'];
+				}
+				$taggedElements = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
+				$table.'.*,COUNT(tx_tagpack_tags_relations_mm.uid_foreign) AS counter',
+					$table.' JOIN tx_tagpack_tags_relations_mm ON (tx_tagpack_tags_relations_mm.uid_foreign='.$table.'.uid AND tx_tagpack_tags_relations_mm.tablenames=\''.$table.'\''.$tagsSelected.')',
+					$table.'.pid>0
+					'.$this->cObj->enableFields($table).'
+					'.$calendarSettings.$searchSettings,
+					'tx_tagpack_tags_relations_mm.uid_foreign',
+					'counter DESC,'.$table.'.uid,'.$table.'.'.$sortingTime.' DESC',
+					$limit );
+			} else {
+				if (count($this->pi1Vars)) {
+					foreach($this->pi1Vars as $value) {
+						if ($value) $enableResultList = true;
+					}
+				}
+				if ($enableResultList || count($getVars)) {
+					$taggedElements = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
+					'*',
+						$table,
+						$table.'.pid>0'.$this->cObj->enableFields($table).'
+						'.$calendarSettings.$searchSettings,
+						$table.'.uid',
+						$table.'.'.($conf['taggedElements.']['timeFields.'][$table] ? $conf['taggedElements.']['timeFields.'][$table] : 'tstamp' ).' DESC');
+				}
+			}
+			if (!$GLOBALS['TYPO3_DB']->sql_error()) {
+				if (count($tags)) {
+					foreach($tags as $key => $tag) {
+						if ($tag['uid'] == t3lib_div::_GET('tx_tagpack_pi3_removeItems')) {
+							unset($tags[$key]);
+						} else {
+							$newUidList .= $newUidList ? ','.$tag['uid'] :
+							 $tag['uid'];
+						}
+					}
+				}
+				$filters = $conf['taggedElements.']['additionalFilters.'][$table.'.'];
+				if (count($tags)) {
+					foreach($tags as $key => $tag) {
+						if (count($tags) > 1) {
+							$linkConf = array(
+							'parameter' => $GLOBALS['TSFE']->id.' - tx_tagack_pi3_removeitem',
+								'additionalParams' => '&tx_tagpack_pi1[uid]='.$newUidList.'&tx_tagpack_pi3_removeItems='.$tag['uid'],
+								'title' => $tag['name'].' '.$this->pi_getLL('remove_item_from_list'),
+								'useCacheHash' => 1,
+								'wrap' => $conf['taggedElements.']['breadcrumbWrap'],
+								);
+						}
+						$tagLink = $this->cObj->typolink($tag['name'], $linkConf);
+						$tagname .= $tagname ? (($key+1) < count($tags) ? ', '.$tagLink : ' '.$this->pi_getLL('and').' '.$tagLink) :
+						$tagLink;
+					}
+				}
+				if(count($filters)) {
+				    foreach($filters as $fieldName => $filterSettings) {
+					    $getVar = t3lib_div::_GET($filterSettings['GETvar']);
+					    if (is_array($getVar) && !$getVar[$filterSettings['GETvar.']['key']]) {
+						$getVar = false;
+					    } else if($getVar[$filterSettings['GETvar.']['key']]) {
+						$getVar = $getVar[$filterSettings['GETvar.']['key']];
+						$linkConf['additionalParams'] .= '&'.$filterSettings['GETvar'].'['.$filterSettings['GETvar.']['key'].']='.$getVar;
+					    } else {
+						$linkConf['additionalParams'] .= '&'.$filterSettings['GETvar'].'='.$getVar;
+					    }
+					    if($getVar) {
+						$headerAppendix[$fieldName] = $filterSettings['label'] ? ' '.$this->cObj->cObjgetSingle($filterSettings['label'],$filterSettings['label.']) : '';
+					    }
+				    }
+				}
+				if ($conf['taggedElements.']['additionalFilters.'][$table.'.']) {
+					$filters = $conf['taggedElements.']['additionalFilters.'][$table.'.'];
+					while($taggedElement = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($taggedElements)) {
+						foreach($filters as $fieldName => $filterSettings) {
+							$getVar = t3lib_div::_GET($filterSettings['GETvar']);
+							if (is_array($getVar) && !$getVar[$filterSettings['GETvar.']['key']]) {
+								$getVar = false;
+							}
+							else if($getVar[$filterSettings['GETvar.']['key']]) {
+								$getVar = $getVar[$filterSettings['GETvar.']['key']];
+							}
+							$fieldName = str_replace('.', '', $fieldName);
+							if ((!$taggedElement[$fieldName] && $getVar) || ($taggedElement[$fieldName]!= $getVar) && $getVar) {
+								unset($taggedElements[$key]);
+							}
+							else if($getVar && $filterSettings['foreign_table'] && !$filterSettings['mm_table'] && !t3lib_div::inList($taggedElement[$fieldName], $getVar)) {
+								unset($taggedElements[$key]);
+							}
+							else if($getVar && $filterSettings['foreign_table'] && $filterSettings['mm_table']) {
+								$availableElementsSelect = $GLOBALS['TYPO3_DB']->exec_SELECT_mm_query(
+								$table.'.uid',
+									$table,
+									$filterSettings['mm_table'],
+									$filterSettings['foreign_table'],
+									' AND '.$table.'.uid='.intval($taggedElement['uid']).' AND '.$filterSettings['foreign_table'].'.uid='.intval($getVar) );
+								if (!$GLOBALS['TYPO3_DB']->sql_num_rows($availableElementsSelect)) {
+									unset($taggedElements[$key]);
+								}
+							}
+						}
+						if ($taggedElement['counter'] >= count($tags)) {
+							$this->localCObj = $this->cObj;
+							$this->localCObj->data = $taggedElement;
+							$itemList .= $this->cObj->wrap($this->localCObj->cObjGetSingle($conf['taggedElements.'][$table], $conf['taggedElements.'][$table.'.']), $conf['taggedElements.']['itemWrap']);
+							$header = $this->pi_getLL('someItems').' '.(
+							$table == 'tt_content' ? $conf['taggedElements.']['contentLabel'] :
+							$conf['taggedElements.']['recordLabels.'][$table]).' '.(
+							$tagUid ? (
+							count($tags) > 1 ? $this->pi_getLL('taggedWith2') :
+							$this->pi_getLL('taggedWith')
+							) : '' ).' '.$tagname;
+						}
+					}
+					$GLOBALS['TYPO3_DB']->sql_free_result($taggedElements);
+				}
+				if (!$header) {
+					$header = $this->pi_getLL('noItems').' '.(
+					$table == 'tt_content' ? $conf['taggedElements.']['contentLabel'] :
+					$conf['taggedElements.']['recordLabels.'][$table]).' '.(
+					$tagUid ? (
+					count($tags) > 1 ? $this->pi_getLL('taggedWith2') :
+					$this->pi_getLL('taggedWith')
+					) : '' ).' '.$tagname;
+				}
+				if(count($headerAppendix)) {
+				    foreach($headerAppendix as $appendixText) {
+					$header .= $appendixText;
+				    }
+				}
+				$content .= $this->cObj->wrap($header, $conf['taggedElements.']['headerWrap']);
+				$content .= $this->cObj->wrap($itemList, $conf['taggedElements.']['groupWrap']);
+				return $content;
+			}
+		}
+	}
+
+
+
+	if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/tagpack/pi3/class.tx_tagpack_pi3.php']) {
+		include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/tagpack/pi3/class.tx_tagpack_pi3.php']);
+	}
+
+?>
